537c5081bef2116e5f2762c19bcc9a0de3be1d15
mcruf <macrufener@gmail.com>
2023-05-14 10:05:33 +0300

    Update folder paths

--- Scripts/PostProcessing/Aggregation_script_02.R
+++ Scripts/PostProcessing/Aggregation_script_02.R
@@ -1,11 +1,11 @@
 ##################################################
 #                                                #
-#         Aggregate car detections data          #
+#         Aggregate car detection data          #
 #                                                #
 ##################################################
 
 
-# The following scriptaggregates the geolocated car detection file,
+# The following scripta ggregates the geolocated car detection file,
 # such that we have the the total number of cars per image.
 # It is essentially meant to be applied once the geolocated car detection files
 # have been cleaned by the OSM_Filtering_script_01.R pipeline - ie, removal of false-positives.
