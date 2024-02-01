088a8716f86e3f5357ac78f3f8ff078403f21cca
mcruf <macrufener@gmail.com>
2024-02-01 16:05:50 -0300

    Overwrite old script version

--- Scripts/Analysis/Exploratory.R
+++ Scripts/Analysis/Exploratory.R
@@ -217,22 +217,64 @@ eastr <- data.frame(xmin='Poltava', xmax='Milove', ymin=-Inf, ymax=Inf)
 
 
 dobs2b$Region <- factor(dobs2b$Region,
-                          levels=c('West','Central', 'East'))
+                        levels=c('West','Central', 'East'))
 
-dobs2b <- dobs2b %>%
-  filter(!(City %in% c('Berdychiv', 'Chortkiv', 'Hremiach', 'Kozyatyn', 'Udobne'))) #We remove these cities because they only have 1 image along the time series (see previous plot)
+# dobs2b <- dobs2b %>%
+#   filter(!(City %in% c('Berdychiv', 'Chortkiv', 'Hremiach', 'Kozyatyn', 'Udobne'))) #We remove these cities because they only have 1 image along the time series (see previous plot)
 
 dobs2b[,c('City', 'Region')] <- lapply(dobs2b[,c('City', 'Region')], factor )
 
 
 ## Define color palette
-cols = rev(brewer.pal(n = 7, name = 'RdBu'))
+# cols = rev(brewer.pal(n = 7, name = 'RdBu'))
+# 
+# dobs2b %>%  
+#   ggplot( aes(x=interaction(City,Region), y=YearMonth, fill=nobs, label=nobs))+
+#   geom_tile(colour = "white", linewidth = 0.5) +
+#   geom_text(col='white') +
+#   scale_fill_gradientn(colours = cols, limits=c(1, 7), na.value="gray95") + 
+#   #scale_fill_gradientn(colours = c("gray95", "blue", "red"), values = c(0,0.2,0.8)) +
+#   #scale_fill_gradient(low="blue", high="red", limits=c(1, 7),na.value="white") +
+#   theme_bw() +
+#   ylab('Time') +  xlab('') + 
+#   labs(fill='No. \nImages') +  
+#   scale_x_discrete(guide = "axis_nested") +
+#   scale_y_discrete(expand = c(0, 0), limits=rev, labels = everysecond(rev(colorder)))  +
+#   # annotate(geom = "text", x = 10, y = 0, label = 'West', size = 6) +
+#   
+#   theme( axis.text.x = element_text(angle = 90,size = 12, vjust = 0.5, hjust = 1, color = c(rep('#2B9EB3', dim(west)[1]), rep('#EFAB1B', dim(central)[1]), rep('#A28377', dim(east)[1]))),
+#          axis.text.y = element_text(size = 12),
+#          axis.title.x = element_text(size = 13, face='bold', margin = unit(c(3, 0, 0, 0), "mm")),
+#          axis.title.y = element_text(size = 13, face='bold', margin = unit(c(0, 3, 0, 0), "mm")),
+#          axis.ticks = element_line(colour = "gray40"),
+#          legend.title.align=0.5,
+#          legend.text = element_text(size = 11),
+#          legend.title = element_text(size = 12, face = 'bold'),
+#          #legend.position = 'bottom',
+#          ggh4x.axis.nestline.x = element_line(linewidth = 0.5),
+#          
+#          ggh4x.axis.nesttext.x = element_text(colour = "gray40", angle = 360, hjust = 0.5, face ='bold',
+#                                               margin = unit(c(3, 0, 0, 0), "mm")))
+
+cols <- c('gray95',
+          "#1f3542",
+          "#86b4d1",
+          "#238d7b",
+          "#f9b339",
+          "#df5940",
+          "#9c100f") #From 0 - 7 images
+
+
+
+dobs2b$nobs <- as.factor(dobs2b$nobs)
+
 
 dobs2b %>%  
   ggplot( aes(x=interaction(City,Region), y=YearMonth, fill=nobs, label=nobs))+
   geom_tile(colour = "white", linewidth = 0.5) +
   geom_text(col='white') +
-  scale_fill_gradientn(colours = cols, limits=c(1, 7), na.value="gray95") + 
+  scale_fill_manual(values = cols, na.value="gray95") +
+  #scale_fill_gradientn(colours = cols, limits=c(1, 7), na.value="gray95") + 
   #scale_fill_gradientn(colours = c("gray95", "blue", "red"), values = c(0,0.2,0.8)) +
   #scale_fill_gradient(low="blue", high="red", limits=c(1, 7),na.value="white") +
   theme_bw() +
@@ -242,19 +284,21 @@ dobs2b %>%
   scale_y_discrete(expand = c(0, 0), limits=rev, labels = everysecond(rev(colorder)))  +
   # annotate(geom = "text", x = 10, y = 0, label = 'West', size = 6) +
   
-  theme( axis.text.x = element_text(angle = 90,size = 12, vjust = 0.5, hjust = 1, color = c(rep('#2B9EB3', dim(west)[1]), rep('#EFAB1B', dim(central)[1]), rep('#A28377', dim(east)[1]))),
-         axis.text.y = element_text(size = 12),
-         axis.title.x = element_text(size = 13, face='bold', margin = unit(c(3, 0, 0, 0), "mm")),
-         axis.title.y = element_text(size = 13, face='bold', margin = unit(c(0, 3, 0, 0), "mm")),
+  theme( axis.text.x = element_text(angle = 90,size = 13, vjust = 0.5, hjust = 1, color = c(rep('#2B9EB3', dim(west)[1]), rep('#EFAB1B', dim(central)[1]), rep('#A28377', dim(east)[1])), face='bold'),
+         axis.text.y = element_text(size = 13, face='bold'),
+         axis.title.x = element_text(size = 14, face='bold', margin = unit(c(3, 0, 0, 0), "mm")),
+         axis.title.y = element_text(size = 14, face='bold', margin = unit(c(0, 3, 0, 0), "mm")),
          axis.ticks = element_line(colour = "gray40"),
          legend.title.align=0.5,
-         legend.text = element_text(size = 11),
-         legend.title = element_text(size = 12, face = 'bold'),
+         legend.text = element_text(size = 12),
+         legend.title = element_text(size = 13, face = 'bold'),
          #legend.position = 'bottom',
          ggh4x.axis.nestline.x = element_line(linewidth = 0.5),
          
          ggh4x.axis.nesttext.x = element_text(colour = "gray40", angle = 360, hjust = 0.5, face ='bold',
                                               margin = unit(c(3, 0, 0, 0), "mm")))
+
+
 
 # ggsave('../../Manuscript/Figures/Exploratory/No_images_per_time.jpg',
 #        dpi = 300, width = 25, height = 20, unit='cm')
